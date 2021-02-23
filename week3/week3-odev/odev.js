86406fa902c2729fb6e37b523852e7594c468413
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-02-23 22:06:44 +0200

    upload

--- week3/week3-odev/odev.js
+++ week3/week3-odev/odev.js
@@ -9,12 +9,18 @@ function hesapmakinesi(sayi1, sayi2, islemisareti){
     
     else if ( islemisareti == '*') console.log(sayi1*sayi2);
     
-    else if ( islemisareti == '/') console.log(sayi1/sayi2);
+    else if ( islemisareti == '/'){
    
-    else console.log('boyle bir islem bulunamadi');
+             if(sayi2==0) console.log('ikinci sayi sifira esit.Bu yuzden sonuc belirsizdir');
+            else console.log(sayi1/sayi2);
+    
+    
+        }
+
+    else console.log('boyle bir islem bulunamadi!!');
 
 }
 
-hesapmakinesi(5,3,'*');
+hesapmakinesi(12,3,'/');
 
 //gelistirilecektir!!
